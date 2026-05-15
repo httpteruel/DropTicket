8a735fc876d0642667ce9a8218e5f1cc5fa4c38d
Arthur Teruel <20230006488@estudantes.ifpr.edu.br>
2026-05-15 20:00:56 -0300

    Update URL patterns for event views

--- dropticket/urls.py
+++ dropticket/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import home
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import home, detalhe_evento
+from core.views import EventoListView, EventoDetailView, EventoCreateView, EventoUpdateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('evento/<int:evento_id>/', detalhe_evento, name='detalhe_evento'),
+    
+    path('', EventoListView.as_view(), name='evento_list'),
+    
+    path('evento/<slug:slug>/', EventoDetailView.as_view(), name='evento_detail'),
+    
+    path('evento/novo/', EventoCreateView.as_view(), name='evento_create'),
+    path('evento/<slug:slug>/editar/', EventoUpdateView.as_view(), name='evento_update'),
 ]
 
 if settings.DEBUG:
