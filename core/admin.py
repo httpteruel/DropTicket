240d5373b2dc2589000b7b87426df72afe8af152
Arthur Teruel <20230006488@estudantes.ifpr.edu.br>
2026-05-15 19:57:15 -0300

    Add ExcursaoInline and update list displays

--- core/admin.py
+++ core/admin.py
@@ -5,17 +5,21 @@ class LoteInline(admin.TabularInline):
     model = Lote
     extra = 1
 
+class ExcursaoInline(admin.TabularInline):
+    model = Excursao
+    extra = 1
+
 @admin.register(Evento)
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'data_inicio', 'local', 'contato_organizador')
+    list_display = ('nome', 'data_inicio', 'local')
     search_fields = ('nome', 'local')
-    inlines = [LoteInline]
+    inlines = [LoteInline, ExcursaoInline]
 
 @admin.register(Excursao)
 class ExcursaoAdmin(admin.ModelAdmin):
-    list_display = ('evento', 'local_saida', 'vagas_totais', 'vagas_ocupadas', 'responsavel')
+    list_display = ('evento', 'local_saida', 'vagas_totais', 'vagas_ocupadas', 'veiculo_tipo')
     list_filter = ('evento', 'local_saida')
-    search_fields = ('responsavel', 'veiculo_placa')
+    search_fields = ('local_saida',)
 
 class ItemPedidoInline(admin.TabularInline):
     model = ItemPedido
@@ -24,6 +28,8 @@ class ItemPedidoInline(admin.TabularInline):
 
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'usuario', 'status', 'data_pagamento', 'total_pedido')
+    list_display = ('id', 'usuario', 'status', 'total_pedido', 'data_criacao')
     list_filter = ('status', 'data_criacao')
     inlines = [ItemPedidoInline]
+
+admin.site.register(Lote)
